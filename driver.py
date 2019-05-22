65a508a14bd785a9a4f62870b0437a4f167b90c3
codingisbliss <madhuratnyc@gmail.com>
2019-05-22 11:42:13 -0400

    Initial code design

---
+++ driver.py
@@ -0,0 +1,3 @@
+from transform_strings import *
+
+wordificationObj = wordification()
---
+++ transform_strings.py
@@ -0,0 +1,11 @@
+class wordification:
+
+	def number_to_words(phNum):
+		pass
+
+	def words_to_number(words):
+		pass
+
+	def all_wordifications():
+		pass
+	
